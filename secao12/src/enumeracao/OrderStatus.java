e8cb3e85cd3728bb726c56199f7f399cee94d43e
Davi Alves <72519831+davialvs@users.noreply.github.com>
2021-02-23 20:06:02 -0300

    initial commit

---
+++ secao12/src/enumeracao/OrderStatus.java
@@ -0,0 +1,9 @@
+package enumeracao;
+
+public enum OrderStatus {
+
+	PENDING_PAYMENT,
+	PROCESING,
+	SHIPPED,
+	DELIVERED;
+}
---
+++ secao12/src/enumeracao/Program.java
@@ -0,0 +1,21 @@
+package enumeracao;
+
+import java.util.Date;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Order order = new Order(108, new Date(), OrderStatus.PENDING_PAYMENT);
+		
+		System.out.println(order);
+		
+		OrderStatus os1 = OrderStatus.DELIVERED; //Intanciando um objeto do tipo OrderStatus com o valor DELIVERED
+		OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
+		
+		System.out.println(os1);
+		System.out.println(os2);
+
+	}
+
+}
