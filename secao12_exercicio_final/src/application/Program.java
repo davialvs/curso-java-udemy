a13d31f3b75843e5980d5316b60a1161bf1d9e94
Davi Alves <72519831+davialvs@users.noreply.github.com>
2021-03-03 22:35:58 -0300

    update composition exercise

--- secao12_exercicio_final/src/application/Program.java
+++ secao12_exercicio_final/src/application/Program.java
@@ -1,10 +1,23 @@
 package application;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 		
+		System.out.println("Enter client data: ");
+		String name = sc.nextLine();
+		System.out.print("E-mail: ");
+		String email = sc.nextLine();
+		System.out.print("Birth date (DD/MM/YYYY): ");
+		String birthdate = sc.nextLine();
+		
+		sc.close();
 
 	}
 
